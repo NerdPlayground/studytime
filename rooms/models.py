e8f882c5b2755b74bade0b0205968556e04cd88a
Paqimm <mcgeewilliam815@gmail.com>
2022-04-08 16:04:58 +0300

    chore (Models) : modify rooms model include uuid on model

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,13 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 class Room(models.Model):
+    id= models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     host= models.ForeignKey(
         'auth.User',
         related_name='rooms',
